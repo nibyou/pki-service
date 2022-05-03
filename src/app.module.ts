2326f4937f797d151a679ef3cc19ebe5c507361d
Luca Kiebel <github@luca-kiebel.de>
2022-05-03 14:34:37 +0200

    Exchange keycloak directory and module file for @nibyou/keycloak NPM Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { KeycloakModule } from './keycloak/keycloak.module';
+import { KeycloakModule } from '@nibyou/keycloak';
 import { KeyModule } from './key/key.module';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 
